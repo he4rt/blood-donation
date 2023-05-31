6a7cbdc63648c82642a937ac881c266dfb10f0dc
Joao Patricio <ijpatricio@gmail.com>
2023-05-31 23:43:53 +0100

    Add production admin tule.

--- .bash_history
+++ .bash_history
@@ -4,3 +4,8 @@ php artisan make:filament-resource Donor/Donor --generate
 php artisan forms:install
 npm install && npm run dev
 exit
+mfs
+a k:g
+mfs
+node -v
+exit
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -42,4 +43,13 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function canAccessFilament(): bool
+    {
+        $adminEmails = [
+            'danielhe4rt@gmail.com',
+        ];
+
+        return in_array($this->email, $adminEmails);
+    }
 }
