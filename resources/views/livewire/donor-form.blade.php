f49cec8930d65ab6b25405e9df2fad6dd2737c2f
m7he4rt <mateuspm2015@hotmail.com>
2023-06-01 01:33:02 -0300

    refactor: button form

--- resources/views/livewire/donor-form.blade.php
+++ resources/views/livewire/donor-form.blade.php
@@ -2,12 +2,12 @@
     <form class="h-auto" wire:submit.prevent="submit">
         {{ $this->form }}
 
-        <button
-            class="p-1 w-full font-heading font-semibold text-xs text-gray-900 uppercase tracking-px overflow-hidden rounded-md">
-            <div class="relative p-5 px-11 bg-gradient-green overflow-hidden rounded-md">
+        <button class="w-full p-1 overflow-hidden text-xs font-semibold text-white uppercase rounded-xl font-heading tracking-px">
+            <div
+                class="relative p-4 overflow-hidden bg-purple-600 border-2 border-purple-600 rounded-md px-11">
                 <div
-                    class="absolute top-0 left-0 transform -translate-y-full group-hover:-translate-y-0 h-full w-full bg-white transition ease-in-out duration-500"></div>
-                <p class="relative z-10">Send message</p>
+                    class="absolute top-0 left-0 w-full h-full transition duration-500 ease-in-out transform -translate-y-full bg-purple-600 group-hover:-translate-y-0"></div>
+                <p class="relative z-10">Enviar</p>
             </div>
         </button>
     </form>
